8ba8f2f65516491a0c0368199c94113fa13360fc
Ale265 <alejoamillano@gmail.com>
2019-11-07 21:04:27 -0300

    Cree carrusel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 window.addEventListener('load', function(){})
+
+// APIKey : c062382504198a6a2cc69f4b0fcd9319
+var queryString = location.search
+var queryStringObj = new URLSearchParams(queryString);
+
+// CARRUSEL
+
+fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=c062382504198a6a2cc69f4b0fcd9319&language=en-US&page=1')
+  .then(function(response) {
+    return response.json();
+    console.log(response);
+ }
+)
+  .then(function(information) {
+    var peliculas = information.results
+    for(var i = 0;i < 5; i++){
+      if
+    }
+  }
+)
+
+// INDEX Series
