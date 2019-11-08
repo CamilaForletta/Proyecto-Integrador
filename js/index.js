be2f97ff209834e7b8300f4d9867f1e74bf312bf
Ale <aamillanorivera@udesa.edu.ar>
2019-11-08 10:05:31 -0300

    asdasd

--- js/index.js
+++ js/index.js
@@ -6,18 +6,37 @@ var queryStringObj = new URLSearchParams(queryString);
 
 // CARRUSEL
 
-fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=c062382504198a6a2cc69f4b0fcd9319&language=en-US&page=1')
+fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=c062382504198a6a2cc69f4b0fcd9319&language=es-AR&page=1')
   .then(function(response) {
     return response.json();
     console.log(response);
- }
-)
+ })
   .then(function(information) {
     var peliculas = information.results
     for(var i = 0;i < 5; i++){
-      if
+      if(i==0)
     }
-  }
-)
+})
+ .catch(function(errores){
+  console.log(errores)
+});
+
 
 // INDEX Series
+
+var arraySeries= document.querySelector()
+
+fetch('https://api.themoviedb.org/3/tv/airing_today?api_key=c062382504198a6a2cc69f4b0fcd9319&language=es-AR&page=1')
+  .then(function(response) {
+    return response.json();
+    console.log(response);
+ })
+ .then(function(information) {
+   var peliculas = information.results
+   for(var i = 0;i < 5; i++){
+     
+   }
+})
+ .catch(function(errores){
+  console.log(errores)
+});
