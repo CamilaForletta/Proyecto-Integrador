e5a2647fed5082c98a99d1057037f90f10dca948
Cami Forletta <cforletta@udesa.edu.ar>
2019-11-16 16:46:29 -0300

    Edité géneros

--- js/detalledeserie.js
+++ js/detalledeserie.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.addEventListener('load', function(){
 var params = (new URL(document.location)).searchParams;
 var id = params.get('id');
 
@@ -91,4 +91,25 @@ fetch("https://api.themoviedb.org/3/tv/"+ id +"/videos?api_key=c062382504198a6a2
   .catch(function(error) {
     console.log("Error: " + error);
   })
-}
+
+  //lista de GENEROS
+  var listaParaGeneros = document.querySelector("#genres-list");
+fetch('https://api.themoviedb.org/3/genre/tv/list?api_key=c062382504198a6a2cc69f4b0fcd9319')
+  .then(function(response) {
+    return response.json();
+ })
+ .then(function(information) {
+   console.log(information)
+   var arrayGeneros = information.genres
+
+   for(var i = 0; i < arrayGeneros.length; i++){
+    listaParaGeneros.innerHTML += `
+      <div><a href=seriesporgenero.html?id=${arrayGeneros[i].id}&name=>${arrayGeneros[i].name}</a></div>
+     `;
+   }
+
+ })
+  .catch(function(errores){
+   console.log(errores)
+ });
+})
--- js/listadegeneros.js
+++ js/listadegeneros.js
@@ -20,4 +20,23 @@ window.addEventListener('load', function(){
     .catch(function(errores){
      console.log(errores)
    });
+   var listaParaGeneros = document.querySelector("#genres-list");
+ fetch('https://api.themoviedb.org/3/genre/tv/list?api_key=c062382504198a6a2cc69f4b0fcd9319')
+   .then(function(response) {
+     return response.json();
+  })
+  .then(function(information) {
+    console.log(information)
+    var arrayGeneros = information.genres
+
+    for(var i = 0; i < arrayGeneros.length; i++){
+     listaParaGeneros.innerHTML += `
+       <div><a href=seriesporgenero.html?id=${arrayGeneros[i].id}&name=>${arrayGeneros[i].name}</a></div>
+      `;
+    }
+
+  })
+   .catch(function(errores){
+    console.log(errores)
+  });
 })
