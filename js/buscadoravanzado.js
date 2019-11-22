8fee1b396400f3ac500449d0238b9fd6dffb9a05
Cami Forletta <cforletta@udesa.edu.ar>
2019-11-21 23:41:51 -0300

    buscador avanzado sin css

--- js/buscadoravanzado.js
+++ js/buscadoravanzado.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', function(){
+//generos
     var listaParaGeneros = document.querySelector("#genres-list");
   fetch('https://api.themoviedb.org/3/genre/tv/list?api_key=c062382504198a6a2cc69f4b0fcd9319')
     .then(function(response) {
@@ -18,4 +19,26 @@ window.addEventListener('load', function(){
     .catch(function(errores){
      console.log(errores)
    });
-})
+     //buscador avanzado
+     var urlListaGeneros = "https://api.themoviedb.org/3/genre/tv/list?api_key=c062382504198a6a2cc69f4b0fcd9319"
+
+
+     fetch(urlListaGeneros)
+       .then(function(response) {
+         return response.json()
+       })
+       .then(function (datos) {
+        console.log(datos)
+        var generoHtml = document.querySelector('#selectGenre')
+        var id = datos.id
+
+        for (var genero of datos.genres) {
+          generoHtml.innerHTML += `
+                <option value=` + genero.id + `> ` + genero.name +  `</option>`;
+        }
+       })
+       .catch(function(errores) {
+           console.log("Error: " + errores);
+       })
+
+     })
---
+++ js/resultadosdelbuscadoravanzado.js
@@ -0,0 +1,58 @@
+window.addEventListener('load',function(){
+
+  var queryStringObj = new URLSearchParams(window.location.search);
+
+
+  var generoBuscado = queryStringObj.get('genre');
+  var añoEstreno = queryStringObj.get('date_release')
+  var ordenVotos = queryStringObj.get('order')
+
+  fetch('https://api.themoviedb.org/3/discover/tv?api_key=c062382504198a6a2cc69f4b0fcd9319&language=en-US&sort_by='+ ordenVotos +'&first_air_date_year='+ añoEstreno +'&page=1&timezone=America%2FNew_York&with_genres=' + generoBuscado + '&without_genres=32&include_null_first_air_dates=false')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(objetoLiteral){
+
+      var resultados = objetoLiteral.results;
+
+      var listaResultados = document.querySelector('#resultado-bavanzado');
+
+      var contenidoFinal = '';
+
+      for (var unResultado of resultados) {
+
+        if (unResultado.poster_path != null) {
+
+          contenidoFinal += '<li>';
+          contenidoFinal += '<img src="https://image.tmdb.org/t/p/original/' + unResultado.poster_path + '"alt="la imagen">';
+          contenidoFinal += '<p>' + unResultado.name + '</p>';
+          contenidoFinal += '<a href="detalle.html?idDeSerie='+ unResultado.id +'">VER MAS</a>';
+          contenidoFinal += '</li>';
+        }
+      }
+
+
+      listaResultados.innerHTML = contenidoFinal;
+
+    })
+    //GENEROS
+    var listaParaGeneros = document.querySelector("#genres-list");
+  fetch('https://api.themoviedb.org/3/genre/tv/list?api_key=c062382504198a6a2cc69f4b0fcd9319')
+    .then(function(response) {
+      return response.json();
+   })
+   .then(function(information) {
+     console.log(information)
+     var arrayGeneros = information.genres
+
+     for(var i = 0; i < arrayGeneros.length; i++){
+      listaParaGeneros.innerHTML += `
+        <div><a href=seriesporgenero.html?id=${arrayGeneros[i].id}&name=>${arrayGeneros[i].name}</a></div>
+       `;
+     }
+
+   })
+    .catch(function(errores){
+     console.log(errores)
+   });
+})
